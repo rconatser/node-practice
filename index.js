3551a5c31e835bf450db956d25316e57fe616ad7
10806252@my.uvu.edu <10806252@my.uvu.edu>
2020-01-06 15:46:09 -0700

    made basic express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', function(res, req, next) {
+    console.log(`A new request was received at ${new Date().toLocaleString()}.`);
+}); // Means "This route... (Default route)"
+
+const port = 8888;
+
+app.listen(port, () => {
+    console.log(`Server is up and running on ${port}.`);
+    res.send(`You have hit the API/Server.`);
+});
+
