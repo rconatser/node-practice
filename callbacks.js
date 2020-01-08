d9e0dfe54b6577f6dbf415d50a93d839097b2194
10806252@my.uvu.edu <10806252@my.uvu.edu>
2020-01-08 15:44:50 -0700

    played around with axios, and async functions

---
+++ callbacks.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+
+const fetchData = callback => { // goes to setTimeout and grabs the console.log (text) function to print the callback after 1.5 seconds
+    setTimeout(() => {
+        callback('FetchData is Done');
+    }, 1500);
+}
+
+setTimeout(() => {
+    console.log('Timer is done!'); // prints text after 2 seconds, then proceeds to function fetchData (below)
+    fetchData(text => { // Passing the text function into "callback" within fetchData which prints the callback text
+        console.log(text);
+    })
+}, 2000);
+
+console.log('Hello');
+console.log('Hi!');
+
+function getSnorlaxData() {
+   return axios.get('https://pokeapi.co/api/v2/pokemon/snorlax')
+  .then(function ({ data }) {
+    // handle success
+    console.log(data.stats);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+}
+
+async function getPikaData() {
+    return axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+    .then(function ({ data }) {
+        // handle success
+        console.log(data.stats);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+}
+
+async function main() {
+    const snorlax = await getSnorlaxData();
+    fs.writeFile('snorlax.json', JSON.stringify(snorlax), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved');
+    })
+    const pika = await getPikaData();
+    console.log(pika);
+}
+
+main();
--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 
 app.use('/', function(res, req, next) {
     console.log(`A new request was received at ${new Date().toLocaleString()}.`);
+    res.send(`Hello World.`);
 }); // Means "This route... (Default route)"
 
 const port = 8888;
 
 app.listen(port, () => {
     console.log(`Server is up and running on ${port}.`);
-    res.send(`Hello World.`);
 });
 
